f82b34df53bc5c5f0838f6502a9b7a1b0f45c2de
ovaphlow <ovaphlow@live.com>
2023-11-22 10:29:11 +0800

    ..

--- events-model.go
+++ events-model.go
@@ -1,11 +1,11 @@
 package main
 
 type Event struct {
+	Id_         string `json:"_id"`
 	Id          int64  `json:"id"`
 	RelationId  int64  `json:"relationId"`
 	ReferenceId int64  `json:"referenceId"`
 	Tags        string `json:"tags"`
 	Detail      string `json:"detail"`
 	Time        string `json:"time"`
-	Id_         string `json:"_id"`
 }
